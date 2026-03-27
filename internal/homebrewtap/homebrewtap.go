b54b9defea3583652b2f4d3706493d8b83418fc8
Rich Clement <richclement@gmail.com>
2026-03-27 16:22:47 -0400

    fix: keep homebrew formula urls templated (#3)

--- internal/homebrewtap/homebrewtap.go
+++ internal/homebrewtap/homebrewtap.go
@@ -54,20 +54,20 @@ var formulaTemplate = template.Must(template.New("formula").Parse(`class {{ .Cla
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/{{ .SourceRepo }}/releases/download/v#{version}/{{ .DarwinARM64.Name }}"
+      url "https://github.com/{{ .SourceRepo }}/releases/download/v#{version}/{{ .FormulaName }}_#{version}_darwin_arm64.tar.gz"
       sha256 "{{ .DarwinARM64.SHA256 }}"
     else
-      url "https://github.com/{{ .SourceRepo }}/releases/download/v#{version}/{{ .DarwinAMD64.Name }}"
+      url "https://github.com/{{ .SourceRepo }}/releases/download/v#{version}/{{ .FormulaName }}_#{version}_darwin_amd64.tar.gz"
       sha256 "{{ .DarwinAMD64.SHA256 }}"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/{{ .SourceRepo }}/releases/download/v#{version}/{{ .LinuxARM64.Name }}"
+      url "https://github.com/{{ .SourceRepo }}/releases/download/v#{version}/{{ .FormulaName }}_#{version}_linux_arm64.tar.gz"
       sha256 "{{ .LinuxARM64.SHA256 }}"
     else
-      url "https://github.com/{{ .SourceRepo }}/releases/download/v#{version}/{{ .LinuxAMD64.Name }}"
+      url "https://github.com/{{ .SourceRepo }}/releases/download/v#{version}/{{ .FormulaName }}_#{version}_linux_amd64.tar.gz"
       sha256 "{{ .LinuxAMD64.SHA256 }}"
     end
   end
